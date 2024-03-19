324ca3e3544c7708ef4304eb9658200ed3299c09
pasindunavod94 <163133054+pasindunavod94@users.noreply.github.com>
2024-03-19 12:03:58 +0530

    Update header.tsx

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,26 +1,49 @@
-import Image from 'next/image';
+"use client";
+import { useAuthStore } from "@/app/store/auth";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
-	return (
-		<header>
-			<nav className='flex items-center bg-rhinoThemeColor shadow-lg h-20'>
-				<div className='w-full 2xl:container 2xl:mx-auto px-8'>
-					<div className='flex items-center'>
-						<div className='logo flex items-center gap-3'>
-							<Image
-								src='/logo.svg'
-								alt='project-x Logo'
-								width={40}
-								height={40}
-								priority
-							/>
-							<h2 className='font-light text-2xl leading-6 text-black'>
-								Project X
-							</h2>
-						</div>
-					</div>
-				</div>
-			</nav>
-		</header>
-	);
+  const router = useRouter();
+  const accessToken = useAuthStore((state) => state?.token?.access_token);
+  const onBtnClick = () => {
+    useAuthStore.setState({
+      token: {
+        token_type: "",
+        access_token: "",
+      },
+    });
+    router.push("/");
+  };
+
+  return (
+    <header>
+      <nav className="flex items-center bg-rhinoThemeColor shadow-lg h-20">
+        <div className="w-full 2xl:container 2xl:mx-auto px-8">
+          <div className="flex justify-between">
+            <div className="logo flex items-center gap-3">
+              <Image
+                src="/logo.svg"
+                alt="project-x Logo"
+                width={40}
+                height={40}
+                priority
+              />
+              <h2 className="font-light text-2xl leading-6 text-black">
+                Project X
+              </h2>
+            </div>
+            {accessToken ? (
+              <button
+                onClick={onBtnClick}
+                className="flex items-end justify-end ali"
+              >
+                log out
+              </button>
+            ) : null}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
